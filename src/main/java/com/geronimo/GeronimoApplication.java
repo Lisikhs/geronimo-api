afb3998df0d3bfa8d45e894f1678daff5fa453eb
Artem <artemthehuman@gmail.com>
2017-08-22 19:55:27 +0300

    initial commit

---
+++ src/main/java/com/geronimo/GeronimoApplication.java
@@ -0,0 +1,12 @@
+package com.geronimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeronimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeronimoApplication.class, args);
+	}
+}
