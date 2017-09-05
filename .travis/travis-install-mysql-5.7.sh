34a5a2a0013ac3e4250c297140cbd26d72d2f9c8
Aleksey Lisikh <lizard5472@gmail.com>
2017-09-06 00:20:29 +0200

    updated script for installation of mysql-5.7 v2

--- .travis/travis-install-mysql-5.7.sh
+++ .travis/travis-install-mysql-5.7.sh
@@ -6,4 +6,4 @@ wget http://dev.mysql.com/get/mysql-apt-config_0.8.6-1_all.deb
 sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.6-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
-sudo mysql_upgrade --force
+sudo mysql_upgrade --force -u root
