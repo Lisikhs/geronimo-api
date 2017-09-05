2c56220033d47f42c5d1c93cae2d3fe1014dc9aa
Aleksey Lisikh <lizard5472@gmail.com>
2017-09-06 00:11:28 +0200

    try to remove mysql-5.6 first and then run installation of mysql-5.7

--- .travis/travis-install-mysql-5.7.sh
+++ .travis/travis-install-mysql-5.7.sh
@@ -1,3 +1,4 @@
+sudo apt purge mysql-server mysql-server-5.6 mysql-server-core-5.6 mysql-client-5.6 mysql-client-core-5.6
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
 wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
 sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
