2801e4dd560a4ac59ef7fa799ed8751e27348917
Aleksey Lisikh <lizard5472@gmail.com>
2017-09-06 00:14:16 +0200

    added shebang line and remove line that removes mysql-5.6, oh my god...

--- .travis/travis-install-mysql-5.7.sh
+++ .travis/travis-install-mysql-5.7.sh
@@ -1,4 +1,5 @@
-sudo apt purge mysql-server mysql-server-5.6 mysql-server-core-5.6 mysql-client-5.6 mysql-client-core-5.6
+#!/usr/bin/env bash
+
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
 wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
 sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
