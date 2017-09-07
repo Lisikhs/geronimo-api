25322103bf638da8a334fd4a812a7e3983031e95
Aleksey Lisikh <lizard5472@gmail.com>
2017-09-07 22:40:54 +0200

    added print classpath method to check it from Travis-CI

--- src/main/java/com/geronimo/GeronimoApplication.java
+++ src/main/java/com/geronimo/GeronimoApplication.java
@@ -1,12 +1,30 @@
 package com.geronimo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URL;
+import java.net.URLClassLoader;
 
 @SpringBootApplication
 public class GeronimoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GeronimoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GeronimoApplication.class, args);
+
+		printClasspath();
+	}
+
+	public static void printClasspath() {
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+		URL[] urls = ((URLClassLoader)cl).getURLs();
+		logger.info("Printing out classpath entries:");
+		for(URL url: urls){
+			logger.info("Classpath entry: " + url.getFile());
+		}
 	}
 }
