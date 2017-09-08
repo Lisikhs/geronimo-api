6a1be35e8d4f65d4381a2dcb8068545726db9a26
Aleksey Lisikh <lizard5472@gmail.com>
2017-09-09 00:51:05 +0200

    #6 updated query for getting user's messages (not sure if it's final though, but at least better)

--- src/main/java/com/geronimo/dao/MessageRepository.java
+++ src/main/java/com/geronimo/dao/MessageRepository.java
@@ -13,8 +13,6 @@ import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    Message findByText(String text);
-
     @Query("select count(l) from Message m join m.likes as l where m.id=:messageId")
     Long countLikes(@Param("messageId") Long messageId);
 
@@ -24,7 +22,6 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     // TODO: change this query as it might hit limit of SQL's IN function on following users
     Page<Message> findByAuthorIn(List<User> followingUsers, Pageable pageable);
 
-    @Query("select m from Message m where m.id in (select m1.id from Message m1 where m1.author = :user)" +
-            "or m.id in (select m2.id from Message m2 where :user member of m2.reblogs)") // select m from Message m where :user member of m.reblogs")
-    Page<Message> findUserMessages(@Param("user")User author, Pageable pageable);
+    @Query("select m from Message m where m.author = :user or :user member of m.reblogs")
+    Page<Message> findUserMessages(@Param("user") User author, Pageable pageable);
 }
