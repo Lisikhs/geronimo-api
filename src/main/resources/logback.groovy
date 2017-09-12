15b992582d44944aa5338d7a530f1126fbbe9d81
Artem <artemthehuman@gmail.com>
2017-09-12 17:37:06 +0300

    replaced xml logback configuration to groovy rollback now works just fine: every day logs are archived archive directory's size can't be bigger than 10MB, so files in it will be deleted automatically when their time comes

---
+++ src/main/resources/logback.groovy
@@ -0,0 +1,21 @@
+import ch.qos.logback.core.util.FileSize
+
+def LOG_PATH = System.getProperty("LOG_FILE")
+def LOG_ARCHIVE = "${LOG_PATH}/archive" as Object
+
+
+appender("RollingFile-Appender", RollingFileAppender) {
+    file = "${LOG_PATH}/spring-boot-logging.log"
+
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "${LOG_ARCHIVE}/rollingfile.log%d{yyyy-MM-dd}.log"
+        maxHistory = 30
+        totalSizeCap = FileSize.valueOf("10MB")
+    }
+
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss} %-4relative [%thread] %-5level %logger{35} - %msg%n"
+    }
+}
+
+root(INFO, ["RollingFile-Appender"])
