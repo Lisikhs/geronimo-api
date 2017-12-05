eed727270032a7dbb4144fdc89c7be0965140ba5
Oleksii Lisikh <lizard5472@gmail.com>
2017-12-05 19:54:58 +0100

    Fixed homeController tests according to security requirements

--- src/test/java/com/geronimo/HomeControllerTest.java
+++ src/test/java/com/geronimo/HomeControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,28 +13,30 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
 @ActiveProfiles("test")
+@WithMockUser(username = "root", roles = { "ADMIN_HOME" })
 public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testHomeController() throws Exception {
-        this.mockMvc.perform(get("/"))
+    public void testAdminEndpoint() throws Exception {
+        mockMvc.perform(get("/admin").with(user("root")))
                 .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+                .andExpect(content().string(containsString("Restricted only for admins")));
     }
 
     @Test
-    public void testIndexPage() throws Exception {
-        this.mockMvc.perform(get("/"))
+    public void testHomeEndpoint() throws Exception {
+        mockMvc.perform(get("/").with(user("root")))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello world!")));
+                .andExpect(content().string(containsString("Hello world")));
     }
 
 }
