0c7673c4d139f162d076cb91410c591f05132489
Oleksii Lisikh <lizard5472@gmail.com>
2018-02-17 22:54:56 +0100

    Tests for MessageController, removed HATEOAS dependency

--- src/main/java/com/geronimo/controller/MessageController.java
+++ src/main/java/com/geronimo/controller/MessageController.java
@@ -2,49 +2,62 @@ package com.geronimo.controller;
 
 import com.geronimo.model.Message;
 import com.geronimo.model.User;
+import com.geronimo.service.IMessageService;
+import com.geronimo.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
-
 
 @RestController
 @RequestMapping("/user/{userId}/message")
-public class MessageController extends AbstractMessageController {
+public class MessageController {
 
-    @PostMapping
-    public void postMessage(@PathVariable("userId") Long userId, @RequestBody Message message,
-                            HttpServletResponse response) {
-        User author = getUserService().getUserById(userId);
-        message.setAuthor(author);
+    private IUserService userService;
+    private IMessageService messageService;
 
-        getMessageService().postMessage(message);
-
-        response.setStatus(HttpStatus.CREATED.value());
+    @Autowired
+    public MessageController(IMessageService messageService, IUserService userService) {
+        this.messageService = messageService;
+        this.userService = userService;
     }
 
-    @GetMapping("/{messageId}")
-    public ResponseEntity<Message> getMessage(@PathVariable("messageId") Long messageId) {
-        Message message = getMessageService().getMessageById(messageId);
+    @PostMapping
+    public ResponseEntity postMessage(@PathVariable("userId") Long userId, @RequestBody Message message) {
+        User author = userService.getUserById(userId);
+        message.setAuthor(author);
+
+        messageService.postMessage(message);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{msgId}")
+    public ResponseEntity<Message> getMessage(@PathVariable("msgId") Long msgId) {
+        Message message = messageService.getMessageById(msgId);
 
         if (message == null) {
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<Page<Message>> getMessages(@PathVariable("userId") Long userId, Pageable pageable) {
-        // get the user
-        User author = getUserService().getUserById(userId);
+    public ResponseEntity<Page<Message>> getMessages(@PathVariable("userId") Long userId,
+                                                     @RequestParam("feed") Boolean feed, Pageable pageable) {
+        User author = userService.getUserById(userId);
 
-        // read user messages and reblogs
-        Page<Message> messages = getMessageService().listUserMessagesAndReblogs(author, pageable);
+        Page<Message> messages;
+        if (feed) {
+            messages = messageService.listFeedMessages(author, pageable);
+        } else {
+            messages = messageService.listUserMessagesAndReblogs(author, pageable);
+        }
+
         return new ResponseEntity<>(messages, HttpStatus.OK);
     }
-
 }
