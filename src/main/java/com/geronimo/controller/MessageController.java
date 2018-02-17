ffbbecaa8c13f36b59585493a3db979cd3230fb3
Oleksii Lisikh <lizard5472@gmail.com>
2018-02-17 16:04:33 +0100

    Removed XML support, we will not use it anyway

--- src/main/java/com/geronimo/controller/MessageController.java
+++ src/main/java/com/geronimo/controller/MessageController.java
@@ -1,35 +1,23 @@
 package com.geronimo.controller;
 
-import com.geronimo.controller.resource.MessageResource;
-import com.geronimo.controller.resource.PageResource;
-import com.geronimo.controller.resource.converter.MessageConverter;
 import com.geronimo.model.Message;
 import com.geronimo.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RestController
 @RequestMapping("/user/{userId}/message")
 public class MessageController extends AbstractMessageController {
 
-    @Autowired
-    private MessageConverter messageConverter;
-
     @PostMapping
-    public void postMessage(@PathVariable("userId") Long userId, @RequestBody MessageResource messageDto,
-                                           HttpServletResponse response) {
-        Message message = messageConverter.convert(messageDto);
-
+    public void postMessage(@PathVariable("userId") Long userId, @RequestBody Message message,
+                            HttpServletResponse response) {
         User author = getUserService().getUserById(userId);
         message.setAuthor(author);
 
@@ -39,35 +27,24 @@ public class MessageController extends AbstractMessageController {
     }
 
     @GetMapping("/{messageId}")
-    public ResponseEntity<MessageResource> getMessage(@PathVariable("messageId") Long messageId) {
+    public ResponseEntity<Message> getMessage(@PathVariable("messageId") Long messageId) {
         Message message = getMessageService().getMessageById(messageId);
 
         if (message == null) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
 
-        MessageResource messageResource = messageConverter.convert(message);
-        return new ResponseEntity<>(messageResource, HttpStatus.OK);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @GetMapping
-    public PageResource<MessageResource> getMessages(@PathVariable("userId") Long userId, Pageable pageable) {
+    public ResponseEntity<Page<Message>> getMessages(@PathVariable("userId") Long userId, Pageable pageable) {
         // get the user
         User author = getUserService().getUserById(userId);
 
         // read user messages and reblogs
         Page<Message> messages = getMessageService().listUserMessagesAndReblogs(author, pageable);
-
-        // convert to renderable object
-        List<MessageResource> messageDtoList = messages.getContent().stream()
-                .map(messageConverter::convert)
-                .collect(Collectors.toList());
-
-        // create a renderable page
-        PageImpl<MessageResource> page = new PageImpl<>(messageDtoList, pageable, messages.getTotalElements());
-
-        // get user messages and reblogs pageable)
-        return new PageResource<>(page);
+        return new ResponseEntity<>(messages, HttpStatus.OK);
     }
 
 }
